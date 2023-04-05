5d8e3ff4212e6e0edbce25870244036da558d417
marotella <michelle.a.rotella@gmail.com>
2023-04-05 19:17:13 -0400

    server dependencies added and running on local host

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-//random code
+//Dependencies
+const express = require("express")
+const cors = require("cors")
+
+//Objects/Variables
+const app = express()
+
+//Middleware
+app.use(cors())
+
+//Routes
+
+//HOME Route:
+app.get("/", (req, res)=>{
+    res.send("Hey!")
+})
+
+
+//PORT
+const PORT = process.env.PORT || 4000
+
+//Listener
+app.listen(PORT, ()=> {
+    console.log("Server listening on "+ PORT)
+})
