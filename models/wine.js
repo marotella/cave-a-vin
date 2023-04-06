1e0f26cf55d0d74917ec776eace7f15186fbcfe9
tiagokim <kt19go@gmail.com>
2023-04-06 08:14:29 -0700

    Mongoose Schema

--- models/wine.js
+++ models/wine.js
@@ -1,1 +1,21 @@
-//wines
+// Import the Mongoose library
+const mongoose = require("mongoose");
+
+// Mongoose schema 
+const WineSchema = new mongoose.Schema({
+    "winery": String,
+    "wine": String,
+    "rating": {
+        "average": String,
+        "reviews": String,
+    },
+    "location": String,
+    "image": String,
+    "id": Number
+});
+
+// Create a Mongoose model based on the Wine schema
+const Wine = mongoose.model("Wine", WineSchema);
+
+// Export the Wine model from the module
+module.exports = Wine;
