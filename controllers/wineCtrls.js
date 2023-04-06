d9ab1448e8b5d5b2b3754667765bbb64f58fa28f
marotella <michelle.a.rotella@gmail.com>
2023-04-06 17:57:36 -0400

    updated index route

--- controllers/wineCtrls.js
+++ controllers/wineCtrls.js
@@ -1,18 +1,35 @@
 //wines
 const db = require("../models")
-
+console.log(db)
 //Route logic
 
+
+//Get Index Route
 const getWine = (req,res) => {
-    // db.Wine.find({})
-    res.send("getWine")
+    db.Wine.find({})
+    .then((foundWine) =>{
+        if(!foundWine){
+            res.status(404).json({message: "Cannot find wine."})
+        }else{
+            res.status(200).json({data: foundPeople})
+        }
+    })
+    
 }
 
+
+//Post Create Route
 const createWine = (req,res) => {
-    //db.Wine.create({name:"testing"})
-    //.then((res)=> {console.log(res)})
+    db.Wine.create({name:"testing"})
+    .then((res)=> {console.log(res)})
     res.send("createWine")
 }
+
+//Post Update Route
+
+
+//Delete Route
+
 
 module.exports = {
     getWine,
