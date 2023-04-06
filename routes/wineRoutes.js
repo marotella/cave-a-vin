d3527249e5c973be16674bf8ea41b7fff7908eca
marotella <michelle.a.rotella@gmail.com>
2023-04-05 20:11:46 -0400

    Setting up routes in routes directory

--- routes/wineRoutes.js
+++ routes/wineRoutes.js
@@ -1,1 +1,9 @@
-//wine routes
+const router =require("express").Router()
+const {wineCtrl} = require("../controllers")
+
+
+//ROUTES:
+router.get("/", wineCtrl.getWine)
+router.post("/", wineCtrl.createWine)
+
+module.exports= router;
