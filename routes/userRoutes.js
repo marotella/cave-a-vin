eed3700a7f96633077a18581b0c9ba0e3fa71357
marotella <michelle.a.rotella@gmail.com>
2023-04-11 22:51:58 -0400

    signin route in progress

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const {userCtrl} = require("../controllers")
 
 //ROUTES:
 router.get("/")  ///form to login or register
-router.post("/signin") //route to login after accessing the form page should be linked to the sign in button.
+router.post("/signin", userCtrl.signin) //route to login after accessing the form page should be linked to the sign in button.
 router.post("/", userCtrl.register) //route to create a new account
 router.get("/signout") //route to logout
 module.exports= router;
