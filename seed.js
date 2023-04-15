69280f15c1fe61490ab203825608eb587a96be6a
marotella <michelle.a.rotella@gmail.com>
2023-04-15 17:46:47 -0400

    data seeded in ATLAS

--- seed.js
+++ seed.js
@@ -1,13 +1,40 @@
-// const db = require("./models");
-// const { seedData } = require("./controllers/wines");
+const mongoose = require('mongoose');
+const Wine = require('./models/wine'); // replace with your own Wine model
+require("dotenv").config()
+const DATABASE_URL = process.env.DATABASE_URL
 
-// db.connect()
-//   .then(async () => {
-//     await seedData();
-//     console.log("Data seeded successfully");
-//     process.exit();
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to database", err);
-//     process.exit(1);
-//   });
+// connect to MongoDB Atlas database
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB Atlas');
+}).catch((err) => {
+  console.log('Error connecting to MongoDB Atlas:', err);
+});
+
+// fetch red wines and insert into database
+fetch('https://api.sampleapis.com/wines/reds')
+  .then((response) => response.json())
+  .then((redWines) => {
+    Wine.insertMany(redWines).then(() => {
+      console.log('Inserted red wines into database');
+    }).catch((err) => {
+      console.log('Error inserting red wines into database:', err);
+    });
+  }).catch((err) => {
+    console.log('Error fetching red wines:', err);
+  });
+
+// fetch white wines and insert into database
+fetch('https://api.sampleapis.com/wines/whites')
+  .then((response) => response.json())
+  .then((whiteWines) => {
+    Wine.insertMany(whiteWines).then(() => {
+      console.log('Inserted white wines into database');
+    }).catch((err) => {
+      console.log('Error inserting white wines into database:', err);
+    });
+  }).catch((err) => {
+    console.log('Error fetching white wines:', err);
+  });
