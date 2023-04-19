56b02474e472606c06d9dfa6f7b84aeb37d335dc
marotella <michelle.a.rotella@gmail.com>
2023-04-18 21:28:02 -0400

    heroku troubleshooting

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 //index
-const mongoose = require('mongoose')
 
 module.exports = {
     Wine: require('./wine')
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 
 const router= require("express").Router()
 const wineRoute = require("./wineRoutes")
-// const userRoute= require("./userRoutes")
-//user 
+
 
 router.use("/wines", wineRoute)
 module.exports = router
