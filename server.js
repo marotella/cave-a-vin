c60a7cbb7d775d3272d4e75213c11acaedd13fd4
marotella <michelle.a.rotella@gmail.com>
2023-04-18 21:44:40 -0400

    troubleshoot

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const app = express()
 const routes = require("./routes/index")
 const MONGODB_URI = process.env.MONGODB_URI
 console.log(MONGODB_URI)
+console.log("testing")
 // connect to MongoDB Atlas database
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false 
+  useUnifiedTopology: true
 }).then(() => {
   console.log('Connected to MongoDB Atlas');
 }).catch((err) => {
