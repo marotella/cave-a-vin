0e795a7a50c749ea192af6aeb36c808a0ce0ec41
marotella <michelle.a.rotella@gmail.com>
2023-04-21 11:31:38 -0400

    updated readme to match front end

--- server.js
+++ server.js
@@ -4,22 +4,29 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require('mongoose');
 const db = mongoose.connection;
+
 //Objects/Variables
-
-
 const app = express()
 const routes = require("./routes/index")
 const MONGODB_URI = process.env.MONGODB_URI
 console.log(MONGODB_URI)
+console.log("testing")
+
+// // connect to MongoDB Atlas database
+// mongoose.connect(MONGODB_URI, {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true
+// }).then(() => {
+//   console.log('Connected to MongoDB Atlas');
+// }).catch((err) => {
+//   console.log('Error connecting to MongoDB Atlas:', err);
+// });
 // connect to MongoDB Atlas database
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true}).then(() => {
+mongoose.connect(MONGODB_URI).then(() => {
   console.log('Connected to MongoDB Atlas');
 }).catch((err) => {
   console.log('Error connecting to MongoDB Atlas:', err);
 });
-
 
 //Middleware
 app.use(cors())
